2ff86e80f52b58f99b1c6efb90005a49f32ec3dd
Jarrett <jarretthall.id@gmail.com>
2021-02-21 12:35:16 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('change message'));
+app.get('/', (req, res) => res.send('module 5'));
 
 app.listen(port);
 add .console.log(`App running on http://localhost:${port}`);
